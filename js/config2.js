053694cc95073bf428b780c72c1d7b00db0d79c5
user <user>
2016-08-10 20:22:43 +0530

    gdf

--- js/config2.js
+++ js/config2.js
@@ -116,3 +116,182 @@ function checkloggedin(uid)
    });
 		
 }
+function checkloggedin(uid)
+{
+	
+	var url=siteurl+'/api/login';
+	
+	jQuery.ajax({  
+	 type: 'POST',  
+	 url: url,  
+	 //contentType: contentType,  
+	 dataType: 'json',  
+	 data: {checkloggedin:1,uid:uid},  
+	 crossDomain: true,  
+	 beforeSend: function() {
+		jQuery('body .bodyoverlay').remove();
+		jQuery('body .preloader').remove();
+		var html='<div class="bodyoverlay"></div><div class="preloader"></div>';
+		jQuery('body').append(html);					
+	 },		
+	 complete: function() {
+		jQuery('body .bodyoverlay').remove();
+		jQuery('body .preloader').remove();					
+	 },
+	 success: function(res) {  
+	   if(res['loggedin']!='success')
+	   {
+		window.location='../index.html';   
+	   }
+		return false;
+	 },  
+	 error: function(response, d, a){
+		jQuery('body .bodyoverlay').remove();
+		jQuery('body .popupbox').remove();
+		var html='<div class="bodyoverlay"></div><div class="popupbox errorbox"><div class="popupimg"><img src="../images/error.png" /></div><h1 class="success">ERROR</h1><h1>Server Error.</h1><button class="okbox">OK</button></div>';
+		jQuery('body').append(html);
+		
+		jQuery('.okbox').click(function(){
+			jQuery('body .bodyoverlay').remove();
+			jQuery('body .popupbox').remove();
+		}); 
+		
+	 } 
+   });
+		
+}
+function activdeactivvehicle()
+{
+	jQuery('.activdeactivvehicle').click(function(){
+		var $this=jQuery(this);
+		var id=jQuery(this).attr('vid');	
+		var mode=jQuery(this).attr('coords');
+		var url=siteurl+'/api/update-vehicle';
+		if(id!='' && mode!='')
+		{
+			jQuery.ajax({  
+			 type: 'POST',  
+			 url: url,  
+			 //contentType: contentType,  
+			 dataType: 'json',  
+			 data: {id:id,mode:mode,uid:uid},  
+			 crossDomain: true,  
+			 beforeSend: function() {
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .preloader').remove();
+				var html='<div class="bodyoverlay"></div><div class="preloader"></div>';
+				jQuery('body').append(html);					
+			 },		
+			 complete: function() {
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .preloader').remove();					
+			 },
+			 success: function(res) {  
+			   if(res['vehicles']['modevehicle'])
+			   {
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+					var html='<div class="bodyoverlay"></div><div class="popupbox"><div class="popupimg"><img src="../images/pop.png" /></div><h1 class="success">SUCCESS</h1><h1>'+res['vehicles']['modevehicle']+'</h1><button class="okbox">OK</button></div>';
+					jQuery('body').append(html);
+				}
+				jQuery('.okbox').click(function(){
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+				}); 
+				if(mode=='1'){
+					jQuery($this).attr('coords',0);
+					jQuery('i',$this).removeClass('fa-check');
+					jQuery('i',$this).addClass('fa-times');
+				}
+				else if(mode=='0'){
+					jQuery($this).attr('coords',1);
+					jQuery('i',$this).removeClass('fa-times');
+					jQuery('i',$this).addClass('fa-check');
+					
+				}
+				return false;
+			 },  
+			 error: function(response, d, a){
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .popupbox').remove();
+				var html='<div class="bodyoverlay"></div><div class="popupbox errorbox"><div class="popupimg"><img src="../images/error.png" /></div><h1 class="success">ERROR</h1><h1>Server Error.</h1><button class="okbox">OK</button></div>';
+				jQuery('body').append(html);
+				
+				jQuery('.okbox').click(function(){
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+				}); 
+				
+			 } 
+		   });
+		}
+	});
+}
+
+function activdeactivuser()
+{
+	jQuery('.activdeactivuser').click(function(){
+		var $this=jQuery(this);
+		var id=jQuery(this).attr('vid');	
+		var mode=jQuery(this).attr('coords');
+		var url=siteurl+'/api/users';
+		if(id!='' && mode!='')
+		{
+			jQuery.ajax({  
+			 type: 'POST',  
+			 url: url,  
+			 //contentType: contentType,  
+			 dataType: 'json',  
+			 data: {id:id,mode:mode,uid:uid},  
+			 crossDomain: true,  
+			 beforeSend: function() {
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .preloader').remove();
+				var html='<div class="bodyoverlay"></div><div class="preloader"></div>';
+				jQuery('body').append(html);					
+			 },		
+			 complete: function() {
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .preloader').remove();					
+			 },
+			 success: function(res) {  
+			   if(res['users']['modeuser'])
+			   {
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+					var html='<div class="bodyoverlay"></div><div class="popupbox"><div class="popupimg"><img src="../images/pop.png" /></div><h1 class="success">SUCCESS</h1><h1>'+res['users']['modeuser']+'</h1><button class="okbox">OK</button></div>';
+					jQuery('body').append(html);
+				}
+				jQuery('.okbox').click(function(){
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+				}); 
+				if(mode=='1'){
+					jQuery($this).attr('coords',0);
+					jQuery('i',$this).removeClass('fa-check');
+					jQuery('i',$this).addClass('fa-times');
+				}
+				else if(mode=='0'){
+					jQuery($this).attr('coords',1);
+					jQuery('i',$this).removeClass('fa-times');
+					jQuery('i',$this).addClass('fa-check');
+					
+				}
+				return false;
+			 },  
+			 error: function(response, d, a){
+				jQuery('body .bodyoverlay').remove();
+				jQuery('body .popupbox').remove();
+				var html='<div class="bodyoverlay"></div><div class="popupbox errorbox"><div class="popupimg"><img src="../images/error.png" /></div><h1 class="success">ERROR</h1><h1>Server Error.</h1><button class="okbox">OK</button></div>';
+				jQuery('body').append(html);
+				
+				jQuery('.okbox').click(function(){
+					jQuery('body .bodyoverlay').remove();
+					jQuery('body .popupbox').remove();
+				}); 
+				
+			 } 
+		   });
+		}
+	});
+}
