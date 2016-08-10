c408f078a3203bcc4fb827f39444f45cd4840d98
user <user>
2016-08-10 20:01:22 +0530

    fs

--- js/config2.js
+++ js/config2.js
@@ -15,31 +15,3 @@ function getUrlParameter(sParam) {
 		}
 	}
 }
-
-function language_en_menus()
-{
-	var menu='<div class="lang_div"><ul><li class="active"><a href="#">English</a></li><li><a href="#">Espanol</a></li></ul></div>';
-	return menu;
-}
-function driver_menus(para='')
-{
-	var menu='<li><a href="violations-towing.html'+para+'">Violation Towing</a></li>';
-		menu+='<li><a href="unauthorized-vehicles.html'+para+'">Unauthorized Vehicles</a></li>';
-		menu+='<li><a href="inactive-vehicles.html'+para+'">Inactive Vehicles</a></li>';
-		menu+='<li><a href="my-account.html'+para+'">My Account</a></li>';
-		menu+='<li><a href="../index.html">Log Out</a></li>';
-	return menu;
-}
-function manager_menus(para='')
-{
-	var menu='<li><a href="vehicles.html'+para+'">Vehicles</a></li>';
-		menu+='<li><a href="violations-towing.html'+para+'">Violation Towing</a></li>';
-		menu+='<li><a href="unauthorized-vehicles.html'+para+'">Unauthorized Vehicles</a></li>';
-		menu+='<li><a href="users.html'+para+'">Users</a></li>';
-		menu+='<li><a href="my-account.html'+para+'">My Account</a></li>';
-		menu+='<li><a href="../index.html">Log Out</a></li>';
-	return menu;
-}
-
-
-
